09f4131be3b9ebb02ba8ce2b1b679be10a36d38d
Yurii Denkovych <yd2004@ukr.net>
2022-06-15 15:52:52 +0300

    Update fetch.js

--- src/fetch.js
+++ src/fetch.js
@@ -1,20 +1,23 @@
 const axios = require('axios');
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import {refs} from './index';
+
 export default class NewLoadApi{
     
-    constructor(){
+   constructor(){
         this.searchAnimals = '';
         this.NUMBER_PAGE = 1;
     }
     
- async  getFetch() {
-    const URL = 'https://pixabay.com/api/?key=28047035-46c6d566ba725b6a4d4d91850&';
+   async  getFetch() {
+         const URL = 'https://pixabay.com/api/?key=28047035-46c6d566ba725b6a4d4d91850&';
     try {
-    const response = await axios.get(`${URL}q=${this.searchAnimals}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.NUMBER_PAGE}&per_page=40`);
+         const response = await axios.get(`${URL}q=${this.searchAnimals}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.NUMBER_PAGE}&per_page=40`);
      
     return response;
     } catch (error) {
-      Notify.failure("We're sorry, but you've reached the end of search results.");
+         refs.btnLoadMore.classList.add('is-hidden')
+         Notify.failure("We're sorry, but you've reached the end of search results.");
     }
   }
 
